ccd3d9eda22ffdf346e3bfb100cc387d03436b0a
Thiago Zanotto <thzanotto@gmail.com>
2022-10-05 22:29:51 -0300

    Primeiro Commit

---
+++ src/test/java/br/edu/unifio/supreagro/supreagro/SupreAgroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.supreagro.supreagro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupreAgroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
